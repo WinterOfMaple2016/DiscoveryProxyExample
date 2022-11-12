cd2a26ce0923a84f43088054fa1df4bfdb31c3af
v-huxiu <v-huxiu@MININT-G8HD94V>
2022-11-12 16:07:47 +0800

    Add project files.

---
+++ Service/CalculatorService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    [ServiceContract(Namespace ="http://Microsoft.Samples.Discovery")]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Add(double n1,double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Received Add({0},{1})", n1,n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            Console.WriteLine("Received Divide({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+    }
+}
---
+++ Service/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Discovery;
+
+namespace Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define the base address of the service
+            Uri baseAddress = new Uri("net.tcp://localhost:9002/CalculatorService/" + Guid.NewGuid().ToString());
+            // Define the endpoint address where announcement messages will be sent
+            Uri announcementEndpointAddress = new Uri("net.tcp://localhost:9021/Announcement");
+
+            // Create the service host
+            ServiceHost serviceHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+            try
+            {
+                // Add a service endpoint
+                ServiceEndpoint netTcpEndpoint = serviceHost.AddServiceEndpoint(typeof(ICalculatorService),
+                    new NetTcpBinding(), string.Empty);
+
+                // Create an announcement endpoint, which points to the Announcement Endpoint hosted by the proxy service.
+                AnnouncementEndpoint announcementEndpoint = new AnnouncementEndpoint(new NetTcpBinding(),
+                    new EndpointAddress(announcementEndpointAddress));
+
+                // Create a ServiceDiscoveryBehavior and add the announcement endpoint
+                ServiceDiscoveryBehavior serviceDiscoveryBehavior = new ServiceDiscoveryBehavior();
+                serviceDiscoveryBehavior.AnnouncementEndpoints.Add(announcementEndpoint);
+
+                // Add the ServiceDiscoveryBehavior to the service host to make the service discoverable
+                serviceHost.Description.Behaviors.Add(serviceDiscoveryBehavior);
+
+                // Start listening for messages
+                serviceHost.Open();
+
+                Console.WriteLine("Calculator Service started at {0}", baseAddress);
+                Console.WriteLine();
+                Console.WriteLine("Press <ENTER> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                serviceHost.Close();
+            }
+            catch (CommunicationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (TimeoutException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (serviceHost.State != CommunicationState.Closed)
+            {
+                Console.WriteLine("Aborting the service...");
+                serviceHost.Abort();
+            }
+        }
+    }
+}
