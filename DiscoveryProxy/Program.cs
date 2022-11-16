7b2357732d477ce45691a0f5627dfda4023d499b
Xiu Hu (Wicresoft) <v-huxiu@microsoft.com>
2022-11-16 14:57:19 +0800

    add more udpversion

--- DiscoveryProxy/Program.cs
+++ DiscoveryProxy/Program.cs
@@ -19,6 +19,21 @@ namespace DiscoveryProxy
                 DiscoveryEndpoint discoveryEndpoint = new DiscoveryEndpoint(new NetTcpBinding(), new EndpointAddress(probeEndpointAddress));
                 discoveryEndpoint.IsSystemEndpoint = false;
 
+                #region Supporting Multiple UDP Discovery Endpoints for different discovery version on single service host
+                //UdpDiscoveryEndpoint newVersionUdpEndpoint = new UdpDiscoveryEndpoint(DiscoveryVersion.WSDiscovery11);
+                //newVersionUdpEndpoint.Address = new EndpointAddress(newVersionUdpEndpoint.Address.Uri.ToString() + "/version11");
+                //proxyServiceHost.AddServiceEndpoint(discoveryEndpoint);
+
+                //UdpDiscoveryEndpoint oldVersionUdpEndpoint = new UdpDiscoveryEndpoint(DiscoveryVersion.WSDiscoveryApril2005);
+                //oldVersionUdpEndpoint.Address = new EndpointAddress(oldVersionUdpEndpoint.Address.Uri.ToString() + "/versionApril2005");
+                //proxyServiceHost.AddServiceEndpoint(oldVersionUdpEndpoint);
+
+
+                //UdpDiscoveryEndpoint otherVersionUdpEndpoint = new UdpDiscoveryEndpoint(DiscoveryVersion.WSDiscoveryCD1);
+                //oldVersionUdpEndpoint.Address = new EndpointAddress(otherVersionUdpEndpoint.Address.Uri.ToString() + "/versionCD1");
+                //proxyServiceHost.AddServiceEndpoint(oldVersionUdpEndpoint);
+                #endregion
+
                 // Add AnnouncementEndpoint to receive Hello and Bye announcement messages
                 AnnouncementEndpoint announcementEndpoint = new AnnouncementEndpoint(new NetTcpBinding(), new EndpointAddress(announcementEndpointAddress));
 
